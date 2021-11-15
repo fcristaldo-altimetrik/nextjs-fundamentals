e96fd12d27dba1dcf571f47e7f279725eeb4d8c0
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-11-15 10:25:58 -0300

    page example

--- pages/user/config/index.tsx
+++ pages/user/config/index.tsx
@@ -1,10 +1,38 @@
 import { NavBar } from "components/NavBar";
-import React from "react";
+import React, { FormEventHandler, useState } from "react";
 
 function UserConfigPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    //do something
+    //axios.post("/api/user/config",{name,email})
+  };
   return (
     <div>
       <NavBar />
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="name">Name </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="email">Email </label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+        </div>
+      </form>
     </div>
   );
 }
