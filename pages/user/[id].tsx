793e3b64f027e4b19709497c3122ce5b38c42bd8
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-11-15 13:00:32 -0300

    client side fetching

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,15 +1,24 @@
-import React from "react";
-import { GetStaticProps, NextPage, GetStaticPaths } from "next";
+import React, { useEffect, useState } from "react";
+import { NextPage } from "next";
 import axios from "axios";
+import { useRouter } from "next/dist/client/router";
 
-interface UserProfileProps {
-  user: {
-    id: string;
-    name: string;
-    email: string;
-  };
+interface User {
+  id?: string;
+  name: string;
+  email: string;
 }
-const UserProfilePage: NextPage<UserProfileProps> = ({ user }) => {
+const UserProfilePage: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [user, setUser] = useState<User | undefined>(undefined);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/users")
+      .then((res) => res.data)
+      .then((data: User[]) => data.filter((d) => d.id === id)[0])
+      .then((u) => setUser(u));
+  }, [id]);
   return (
     <div>
       <h6>{user?.id}</h6>
@@ -17,38 +26,6 @@ const UserProfilePage: NextPage<UserProfileProps> = ({ user }) => {
       <h6>{user?.email}</h6>
     </div>
   );
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  //fetch user from API or DB
-  const users = (await axios.get("http://localhost:3000/api/users")).data || [];
-  const user = users.filter((u: { id: string }) => u.id == params?.id)[0];
-  console.log(users, user);
-  if (user) {
-    return {
-      props: { user: { ...user } }, // will be passed to the page component as props
-      revalidate: false, // seconds or false: seconds to re-render the page
-      notFound: false, // boolean to indicate if not-found page should be returned
-    };
-  }
-  return {
-    props: { user: {} },
-    notFound: true,
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  // Call an external API endpoint to get posts
-  const users: { id: string; name: string; email: string }[] =
-    (await axios.get("http://localhost:3000/api/users")).data || [];
-
-  // Get the paths we want to pre-render based on posts
-  const paths = users.map((user) => ({
-    params: { id: user.id },
-  }));
-
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: true };
 };
 
 export default UserProfilePage;
