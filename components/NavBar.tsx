41723433f4c00720a331feb2ae50f0feb78c9fd9
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-11-15 09:14:17 -0300

    sass intgration and example

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "styles/NavBar.module.scss";
 
 export const NavBar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Image src="/vercel.svg" alt="Logo" width={100} height={75} />
-      <nav>
-        <Link href="#">Home</Link>
+      <nav className={styles.navbar}>
+        <div className={styles.item}>
+          <Link href="#">Home</Link>
+        </div>
+        <div className={styles.item}>
+          <Link href="#">Profile</Link>
+        </div>
+        <div className={styles.item}>
+          <Link href="#">LogIn</Link>
+        </div>
       </nav>
     </div>
   );
