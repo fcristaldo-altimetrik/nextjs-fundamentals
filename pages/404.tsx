3a50addbf25e822d93465647c10ce17ec60326a5
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-12-01 19:41:23 -0300

    not found page example

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className={"center-content"}>Ups! We couldnt found this page</div>
+  );
+};
+
+export default NotFoundPage;
